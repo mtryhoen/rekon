697f8434f91aa15941673500499f86a340ab827e
mtryhoen <mtryhoen@gmail.com>
2017-11-17 18:13:57 +0100

    aws sdk v0

--- flaskapp/static/awsCred.js
+++ flaskapp/static/awsCred.js
@@ -1,13 +1,18 @@
 function awsCred() {
-    AWS.config.region = 'eu-west-1'; // Region
-    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-        IdentityPoolId: 'eu-west-1:5c811920-9ed9-4713-93b4-ce5ae70de196',
-    });
-    AWS.config.credentials.get(function(){
-
-        // Credentials will be available when this function is called.
-        var accessKeyId = AWS.config.credentials.accessKeyId;
-        var secretAccessKey = AWS.config.credentials.secretAccessKey;
-        var sessionToken = AWS.config.credentials.sessionToken;
-    });
+    console.log('Function');
+//    AWS.config.region = 'eu-west-1'; // Region
+//    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+//        IdentityPoolId: 'eu-west-1:5c811920-9ed9-4713-93b4-ce5ae70de196',
+//    });
+//    AWS.config.credentials.get(function(){
+//
+//        // Credentials will be available when this function is called.
+//        var accessKeyId = AWS.config.credentials.accessKeyId;
+//        var secretAccessKey = AWS.config.credentials.secretAccessKey;
+//        var sessionToken = AWS.config.credentials.sessionToken;
+//
+//        console.log(accessKeyId);
+//        console.log(secretAccessKey);
+//        console.log(sessionToken);
+//    });
 }
